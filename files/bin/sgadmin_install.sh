e86ed1d93201d9dd602f5d157e31b1e1b18b01cb
Pape BADIANE <pape.badiane@openio.io>
2018-07-18 10:53:10 +0200

    Wait for Elasticsearch startup before to set up search-guard

--- files/bin/sgadmin_install.sh
+++ files/bin/sgadmin_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 /usr/share/elasticsearch/plugins/search-guard-6/tools/sgadmin.sh \
-	-cd ../sgconfig/ \
-	-ts ../sgconfig/truststore.jks \
+	-cd /etc/elasticsearch/instance1/sgconfig/ \
+	-ts /etc/elasticsearch/instance1/sgconfig/truststore.jks \
 	-tspass changeit \
-	-ks ../sgconfig/kirk-keystore.jks  \
+	-ks /etc/elasticsearch/instance1/sgconfig/kirk-keystore.jks  \
 	-kspass changeit \
 	-nhnv \
 	-icl
