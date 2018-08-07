fbaa57a4a7a2f30195cae4a7d04eeb93e15a73db
Pape BADIANE <pape.badiane@openio.io>
2018-08-07 15:46:33 +0200

    Initial commit for test

--- files/bin/sgadmin_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-/usr/share/elasticsearch/plugins/search-guard-6/tools/sgadmin.sh \
-	-cd /etc/elasticsearch/instance1/sgconfig/ \
-	-ts /etc/elasticsearch/instance1/sgconfig/truststore.jks \
-	-tspass changeit \
-	-ks /etc/elasticsearch/instance1/sgconfig/kirk-keystore.jks  \
-	-kspass changeit \
-	-nhnv \
-	-icl
